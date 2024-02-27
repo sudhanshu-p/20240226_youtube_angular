bc6104a57c0ed427fcfade6a1f3fc612ab4e84dc
sudhanshu-p <pandeysv@rknec.edu>
2024-02-27 17:51:33 +0530

    Added the settings page, created routing for the same.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/homepage/home.component';
 import { SubscriptionsComponent } from './pages/subscriptionspage/subscriptions.component';
+import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
 
 export const routes: Routes = [
     { path: "", component: HomeComponent },
-    { path: "sub", component: SubscriptionsComponent }
+    { path: "sub", component: SubscriptionsComponent },
+    { path: "settings", component: SettingsPageComponent }
 ];
